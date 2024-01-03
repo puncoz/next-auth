15dc2306eb372653b4246706fafbc0ccdf790e8f
Puncoz Nepal <puncoz138@gmail.com>
2024-01-04 02:15:59 +0900

    next auth callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,15 @@
-import { signOut } from "@/auth/auth"
+import { auth, signOut } from "@/auth/auth"
 import { Button } from "@/components/ui/button"
 import React from "react"
 
 type Props = {};
 
 const SettingsPage = async (props: Props) => {
+  const session = await auth()
+
   return (
     <div className="h-screen bg-slate-800 text-white">
+      {JSON.stringify(session)}
       <form action={async () => {
         "use server"
 
--- auth/auth.ts
+++ auth/auth.ts
@@ -1,6 +1,8 @@
 import authConfig from "@/auth/auth.config"
+import { getUserById } from "@/data/user"
 import { db } from "@/lib/db"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { UserRole } from "@prisma/client"
 import NextAuth from "next-auth"
 
 export const {
@@ -9,6 +11,44 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    signIn: async ({ user }) => {
+      const dbUser = await getUserById(user.id)
+
+      return !(!dbUser || !dbUser.email_verified_at)
+    },
+
+    session: async ({ token, session }) => {
+      if (!session.user) {
+        return session
+      }
+
+      if (token.sub) {
+        session.user.id = token.sub
+      }
+
+      if (token.role) {
+        session.user.role = token.role as UserRole
+      }
+
+      return session
+    },
+
+    jwt: async ({ token }) => {
+      if (!token.sub) {
+        return token
+      }
+
+      const dbUser = await getUserById(token.sub)
+      if (!dbUser) {
+        return token
+      }
+
+      token.role = dbUser.role
+
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
